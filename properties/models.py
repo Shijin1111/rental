7a7d7900ff8a74c2cb51d030ae86b68134cf7480
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-03-17 21:42:13 +0530

    properties available

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Property(models.Model):
+    property_id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    area = models.IntegerField()
+    floor = models.IntegerField()
+    location = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    cost = models.IntegerField()
+    bedrooms = models.IntegerField()
+    kitchen = models.IntegerField()
+    hall = models.CharField(max_length=3)  # 'Yes' or 'No'
+    balcony = models.CharField(max_length=3)  # 'Yes' or 'No'
+    description = models.CharField(max_length=200)
+    AC = models.CharField(max_length=3)  # 'Yes' or 'No'
+    image = models.ImageField(upload_to='property_images/', blank=True, null=True)
+    date_added = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.property_id} - {self.location}, {self.city}"
